c8c31efd894228ebc130845aacd847bdfb969d3a
Takuma Ohtani <ohtani.takuma@htncode.com>
2024-03-21 00:12:16 +0900

    commit202403210012

--- templates-sample/app.py
+++ templates-sample/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
@@ -50,7 +50,11 @@ class Hero:
     # 表示用関数
     def show(self):
         return f"名前：{self.name} 年齢：{self.age}"
-
+    
+    # __str__メソッドをオーバーライド（これをしないとオブジェクトが文字列として表示されない）
+    def __str__(self):
+        return self.show()
+    
 @app.route("/class/")
 def show_jinja_class():
     taro = Hero("太郎", 20)
@@ -92,6 +96,50 @@ def show_jinja_if(target="colorless"):
 def show_filter_block():
     word = "pen"
     return render_template("filter/block.html", show_word=word)
+
+# フィルター（変数）
+@app.route("/filter_var/")
+def show_filter_variable():
+    # クラスを作成
+    momo = Hero("桃太郎", 20)
+    kinta = Hero("金太郎", 25)
+    ura = Hero("浦島太郎", 30)
+    kagu = Hero("かぐや姫", 35)
+    kasa = Hero("かさ地蔵", 40)    
+    # リストに格納
+    hero_list = [momo, kinta, ura, kagu, kasa]
+    return render_template("filter/filter_list.html", heroes = hero_list)
+
+# カスタム（自作）フィルターは以下のようにする
+@app.template_filter("custom_filter")
+def custom_filter(value, length=10):
+    if len(value) > length:
+        return value[:length] + "..."
+    else:
+        return value
+
+# カスタム（自作）フィルターを使う
+@app.route("/custom_filter/")
+def show_custom_filter():
+    word = "カスタムフィルターを使ってみる"
+    return render_template("filter/custom_filter.html", show_word=word, show_word2=word)
+
+# エラーハンドリングのモジュールのインポート
+from werkzeug.exceptions import NotFound
+
+# エラーハンドリング
+@app.errorhandler(NotFound)
+def show_404_page(error):
+    msg = error.description
+    print("エラー内容：", msg) # ステータスコード別のエラー内容を確認したい場合は、NotFoundをCtrl+クリックして確認できる
+    return render_template("errors/404.html"), 404
+
+# abort関数を使って例外を発生させる（これによって、明示的に404エラーを発生させ、その際に表示するメッセージ【error.description】を指定できる）
+@app.route("/abort/")
+def create_exception():
+    abort(404, "要求されたページやファイルが見つかりません")
+
+
 
 # 実行
 if __name__ == "__main__":
